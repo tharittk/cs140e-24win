74b71dea7fe2eb479def996bdcf72e9e196b878f
tharittk <tharit.tangkij@gmail.com>
2025-09-14 14:00:42 +0700

    lab2 step1

--- labs/2-trusting-trust/code/step1/quine-gen.c
+++ labs/2-trusting-trust/code/step1/quine-gen.c
@@ -4,6 +4,21 @@
 #include <stdio.h>
 
 int main(void) { 
-    // put your code here.
+    int i = 0;
+    // risk buffer overflow
+    char prog[5000]; // enough for this seed.c
+
+	printf("char prog[] = {\n");
+    int c;
+    while ((c = getchar()) != EOF){
+		printf("\t%d,%c", c, (i+1)%8==0 ? '\n' : ' ');
+        prog[i] = c;
+        ++i;
+    }
+    prog[i] = '\0';
+	printf("0 };\n");
+    // line below makes it so that the output file
+    // is compilable C file (header, main, etc.)
+	printf("%s", prog);
 	return 0;
 }
--- labs/2-trusting-trust/code/step1/seed.c
+++ labs/2-trusting-trust/code/step1/seed.c
@@ -6,6 +6,16 @@ int main() {
 	int i;
 
  	// Q: why can't we just print prog twice?
+	// A: printint program twice and you will get
+	// two char prog[] = {...}. It cannot be compiled as
+	// it is not a valid C code. The printf("%s", prog)
+	// translate the char array to C code.
+	// No. I think the means
+	// prinf("%s", prog);
+	// prinf("%s", prog);
+	// That will make your program invalid as it has 2 main()
+	// quine.c is not compilable then.
+
 	printf("char prog[] = {\n");
 	for(i = 0; prog[i]; i++)
 		printf("\t%d,%c", prog[i], (i+1)%8==0 ? '\n' : ' ');
