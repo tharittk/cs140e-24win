4f37ea80c75999de0ca9f5047659b9fc1d010253
tharittk <tharit.tangkij@gmail.com>
2025-10-12 17:29:31 +0700

    trace real-pi works

--- labs/4-cross-checking/2-trace/trace-notmain.c
+++ labs/4-cross-checking/2-trace/trace-notmain.c
@@ -4,8 +4,9 @@
 void __wrap_notmain(void);
 void __real_notmain(void);
 
-void __wrap_notmain(void)
-{
-    // implement this function!
-    __real_notmain();
+void __wrap_notmain(void) {
+  // implement this function!
+
+  printk("TRACE:calling pi code\n");
+  __real_notmain();
 }
