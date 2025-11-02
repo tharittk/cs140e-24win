6f707d5bd6352e2d605b27a2564ce3890e8b8729
tharittk <tharit.tangkij@gmail.com>
2025-11-02 09:23:08 +0700

    gprof 1st draft

--- labs/5-interrupts/1-gprof/gprof.c
+++ labs/5-interrupts/1-gprof/gprof.c
@@ -1,7 +1,7 @@
 /*
  * use interrupts to implement a simple statistical profiler.
  *	- interrupt code is a replication of ../timer-int/timer.c
- *	- you'll need to implement kmalloc so you can allocate 
+ *	- you'll need to implement kmalloc so you can allocate
  *	  a histogram table from the heap.
  *	- implement functions so that given a pc value, you can increment
  *	  its associated count
@@ -11,7 +11,7 @@
 
 // defines C externs for the labels defined by the linker script
 // libpi/memmap.
-// 
+//
 // you can use these to get the size of the code segment, data segment,
 // etc.
 #include "memmap.h"
@@ -26,23 +26,37 @@
 
 // allocate table.
 //    few lines of code
+static uint32_t *table;
+static uint32_t code_size_bytes;
 static unsigned gprof_init(void) {
-    unimplemented();
+  code_size_bytes = (__code_end__ - __code_start__) * 4;
+  table = (uint32_t *)kmalloc(code_size_bytes);
+  for (int i = 0; i < code_size_bytes / 4; ++i) {
+    table[i] = 0;
+  }
+  return 0;
 }
 
 // increment histogram associated w/ pc.
 //    few lines of code
 static void gprof_inc(unsigned pc) {
-    unimplemented();
+  unsigned index = ((uint32_t)pc - (uint32_t)__code_start__) / 4;
+  ++table[index];
 }
 
 // print out all samples whose count > min_val
 //
 // make sure sampling does not pick this code up!
+#define arm_timer_Control 0x2000B408
 static void gprof_dump(unsigned min_val) {
-    unimplemented();
+  printk("---\n");
+  for (int i = 0; i < code_size_bytes / 4; ++i) {
+    if (table[i] > min_val) {
+      uint32_t addr = (uint32_t)__code_start__ + i * 4;
+      printk("pc at %x : %d \n", addr, table[i]);
+    }
+  }
 }
-
 
 /***********************************************************************
  * timer interrupt code from before, now calls gprof update.
@@ -53,65 +67,65 @@ static volatile unsigned period;
 
 // client has to define this.
 void interrupt_vector(unsigned pc) {
-    dev_barrier();
-    unsigned pending = GET32(IRQ_basic_pending);
+  dev_barrier();
+  unsigned pending = GET32(IRQ_basic_pending);
 
-    // if this isn't true, could be a GPU interrupt (as discussed in Broadcom):
-    // just return.  [confusing, since we didn't enable!]
-    if((pending & RPI_BASIC_ARM_TIMER_IRQ) == 0)
-        return;
-    /* 
-     * Clear the ARM Timer interrupt - it's the only interrupt we have
-     * enabled, so we want don't have to work out which interrupt source
-     * caused us to interrupt 
-     *
-     * Q: if we delete?
-     */
-    PUT32(arm_timer_IRQClear, 1);
-    cnt++;
+  // if this isn't true, could be a GPU interrupt (as discussed in Broadcom):
+  // just return.  [confusing, since we didn't enable!]
+  if ((pending & RPI_BASIC_ARM_TIMER_IRQ) == 0)
+    return;
+  /*
+   * Clear the ARM Timer interrupt - it's the only interrupt we have
+   * enabled, so we want don't have to work out which interrupt source
+   * caused us to interrupt
+   *
+   * Q: if we delete?
+   */
+  PUT32(arm_timer_IRQClear, 1);
+  cnt++;
 
-    // increment the counter for <pc>.
-    gprof_inc(pc);
+  // increment the counter for <pc>.
+  gprof_inc(pc);
 
-    // this doesn't need to stay here.
-    static unsigned last_clk = 0;
-    unsigned clk = timer_get_usec();
-    period = last_clk ? clk - last_clk : 0;
-    last_clk = clk;
-	
-    // Q: if we put a print statement?
+  // this doesn't need to stay here.
+  static unsigned last_clk = 0;
+  unsigned clk = timer_get_usec();
+  period = last_clk ? clk - last_clk : 0;
+  last_clk = clk;
+
+  // Q: if we put a print statement?
 }
 
 // trivial program to test gprof implementation.
 // 	- look at output: do you see weird patterns?
 void notmain() {
-    printk("about to install handlers\n");
-    int_init();
+  printk("about to install handlers\n");
+  int_init();
 
-    printk("setting up timer interrupts\n");
-    // Q: if you change 0x100?
-    timer_interrupt_init(0x10);
+  printk("setting up timer interrupts\n");
+  // Q: if you change 0x100?
+  timer_interrupt_init(0x10);
 
-    printk("gonna enable ints globally!\n");
+  printk("gonna enable ints globally!\n");
 
-    // Q: if you move these below interrupt enable?
-    uint32_t oneMB = 1024*1024;
-    kmalloc_init_set_start((void*)oneMB, oneMB);
-    gprof_init();
+  // Q: if you move these below interrupt enable?
+  uint32_t oneMB = 1024 * 1024;
+  kmalloc_init_set_start((void *)oneMB, oneMB);
+  gprof_init();
 
-    // Q: if you don't do?
-    printk("gonna enable ints globally!\n");
-    system_enable_interrupts();
-    printk("enabled!\n");
+  // Q: if you don't do?
+  printk("gonna enable ints globally!\n");
+  system_enable_interrupts();
+  printk("enabled!\n");
 
-    // caches_enable(); 	// Q: what happens if you enable cache?
-    unsigned iter = 0;
-    while(cnt<200) {
-        printk("iter=%d: cnt = %d, period = %dusec, %x\n",
-                iter,cnt, period,period);
-        iter++;
-        if(iter % 10 == 0)
-            gprof_dump(2);
-    }
-    clean_reboot();
+  // caches_enable(); 	// Q: what happens if you enable cache?
+  unsigned iter = 0;
+  while (cnt < 200) {
+    // printk("iter=%d: cnt = %d, period = %dusec, %x\n", iter, cnt, period,
+    //    period);
+    iter++;
+    if (iter % 10 == 0)
+      gprof_dump(2);
+  }
+  clean_reboot();
 }
