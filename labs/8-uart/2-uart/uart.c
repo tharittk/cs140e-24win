8cd47469bbfcd78ec28bde00039eaea1c95d5307
tharittk <tharit.tangkij@gmail.com>
2025-11-26 22:50:25 +0700

    uart almost ok. to fix disable test

--- labs/8-uart/2-uart/uart.c
+++ labs/8-uart/2-uart/uart.c
@@ -13,6 +13,7 @@
 //
 //
 #include "rpi.h"
+#include "sw-uart.h"
 
 #define AUX_IRQ  0x20215000
 #define AUX_ENB  0x20215004
@@ -48,33 +49,43 @@ static int rx_pin = 15;
 //
 //  later: should add an init that takes a baud rate.
 void uart_init(void) {
+
+    // sw_uart_t sw = sw_uart_default();
+    // disable first because it is enabled by bootloader
+    PUT32(AUX_ENB, GET32(AUX_ENB) & ~1); // Clear bit 0 to disable AUX UART1
+    dev_barrier();
+
     // set gpio pin, in sw uart, we set it as output and input pin.
     gpio_set_function(tx_pin, GPIO_FUNC_ALT5);
     gpio_set_function(rx_pin, GPIO_FUNC_ALT5);
+    dev_barrier();
 
-    assert(gpio_read(rx_pin) == 1);
-    assert(gpio_read(tx_pin) == 1);
     // turn on the global enabler - do read-modify-write (protect SPIm)
     PUT32(AUX_ENB, GET32(AUX_ENB) | 0b1);
+    dev_barrier();
 
     // disable tx, rx
-    PUT32(&MU->CNTL, GET32(&MU->CNTL) & ~(0b11));
+    PUT32(&MU->CNTL, GET32(&MU->CNTL) & ~0b11); // Clear bits 0 and 1 to disable TX/RX
+    dev_barrier();
 
     // clear FIFO state for both rx, tx
     PUT32(&MU->IIR, 0b110); 
+    dev_barrier();
 
-    // TODO: disable interrupt
+    // disable interrupt
     // make sure DLAB == 0
+    PUT32(&MU->IER, GET32(&MU->IER) & ~(0b11)); // clear last two bit
 
-
-    // configure 115,200 baudrate, 8n1
-    PUT32(&MU->BAUD, 115200);
+    // configure 115,200 baudrate, 8n1, using 250 Mhz clock
+    PUT32(&MU->BAUD, 270);
 
     // 8bit mode
-    PUT32(&MU->LCR, GET32(&MU->LCR) | 0b1);
+    PUT32(&MU->LCR, GET32(&MU->LCR) | 0b11);
+    dev_barrier();
 
     // enable tx, rx
     PUT32(&MU->CNTL, GET32(&MU->CNTL) | (0b11));
+    // sw_uart_printk(&sw, "end of init\n");
 }
 
 // disable the uart.
@@ -82,24 +93,23 @@ void uart_disable(void) {
 }
 
 int uart_can_get8(void){
-    // redundant check 2 regs ?
-    // rx FIFO hold at least one symbol
-    return (GET32(&MU->LSR) & 1) && (GET32(&MU->STAT) & 1);
+    // Check LSR bit 0 (Data Ready)
+    return GET32(&MU->LSR) & 1;
 }
 
 // returns one byte from the rx queue, if needed
 // blocks until there is one.
 int uart_get8(void) {
-    while (!uart_can_get8())
+    while (!uart_can_get8()) {
         ;
+    }
     return (int) GET8(&MU->IO);
 }
 
 // 1 = space to put at least one byte, 0 otherwise.
 int uart_can_put8(void) {
-    // redundant check 2 regs ?
-    // tx FIFO can take at least one byte
-    return (GET32(&MU->LSR) & (1 << 5)) && (GET32(&MU->STAT) & (1 << 1));
+    // Check LSR bit 5 (Transmitter Empty)
+    return GET32(&MU->LSR) & (1 << 5);
 }
 
 // put one byte on the tx qqueue, if needed, blocks
@@ -116,7 +126,7 @@ int uart_put8(uint8_t c) {
 
 // 1 = at least one byte on rx queue, 0 otherwise
 int uart_has_data(void) {
-    todo("Must implement");
+    return uart_can_get8();
 }
 
 // return -1 if no data, otherwise the byte.
